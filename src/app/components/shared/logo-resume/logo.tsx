7b5fdd3916000954b0d89d1405eefd7aab5ab2f8
Miriam Elizabeth Laguna Velazquez <EliLV21@users.noreply.github.com>
2026-01-29 19:15:39 -0600

    modify logo path

--- src/app/components/shared/logo-resume/logo.tsx
+++ src/app/components/shared/logo-resume/logo.tsx
@@ -1,17 +1,17 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import logo from '@/assets/logo/EliLV.png';
 
 export const LogoIcon = () => {
   const isProd = process.env.NODE_ENV === 'production';
+  const basePath = isProd ? '/profile-resume' : '';
   return (
-    <Link href={`${isProd ? '/profile-resume' : '/'}`} style={{ zIndex: 100 }} className="absolute left-4 top-4">
+    <Link href={`${basePath}/`} style={{ zIndex: 100 }} className="absolute left-4 top-4">
       <Image
         className=""
         alt="Logo"
         width={200}
         height={200}
-        src={logo}
+        src="/logo/EliLV.png"
       />
     </Link>
   );
