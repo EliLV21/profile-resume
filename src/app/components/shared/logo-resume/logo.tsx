cd358062633a692a6b7e50b4d78c2d0f625010a4
Miriam Elizabeth Laguna Velazquez <EliLV21@users.noreply.github.com>
2026-03-05 17:52:30 -0600

    save changes

--- src/app/components/shared/logo-resume/logo.tsx
+++ src/app/components/shared/logo-resume/logo.tsx
@@ -1,15 +1,16 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import logoImage from '@/assets/logo/EliLV.png';
 
 export const LogoIcon = () => {
   return (
-    <Link href="/" style={{ zIndex: 100 }} className="absolute left-4 top-4">
+    <Link href="/" style={{ zIndex: 100 }} className="absolute left-4 top-4 z-20">
       <Image
         className=""
         alt="Logo"
         width={200}
         height={200}
-        src="/profile-resume/_next/static/media/logo/EliLV.png"
+        src={logoImage}
       />
     </Link>
   );
